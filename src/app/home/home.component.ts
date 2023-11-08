9065c817de20e3d71e47d160266aabc144207d4c
Aliz <alicja_bobko@o2.pl>
2023-11-08 12:49:51 +0100

    stateless observable services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,38 +23,30 @@ import { CoursesService } from "../services/courses.services";
   styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
-  beginnerCourses: Course[];
+  beginnerCourses$: Observable<Course[]>;
 
-  advancedCourses: Course[];
+  advancedCourses$: Observable<Course[]>;
 
-  constructor(
-    private coursesService: CoursesService,
-    private dialog: MatDialog
-  ) {}
+  constructor(private coursesService: CoursesService) {}
 
   ngOnInit() {
-    this.http.get("/api/courses").subscribe((res) => {
-      const courses: Course[] = res["payload"].sort(sortCoursesBySeqNo);
-
-      this.beginnerCourses = courses.filter(
-        (course) => course.category == "BEGINNER"
-      );
-
-      this.advancedCourses = courses.filter(
-        (course) => course.category == "ADVANCED"
-      );
-    });
+    this.reloadCourses();
   }
 
-  editCourse(course: Course) {
-    const dialogConfig = new MatDialogConfig();
+  reloadCourses() {
+    const courses$ = this.coursesService
+      .loadAllCourses()
+      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo), shareReplay()));
 
-    dialogConfig.disableClose = true;
-    dialogConfig.autoFocus = true;
-    dialogConfig.width = "400px";
-
-    dialogConfig.data = course;
-
-    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig);
+    this.beginnerCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "BEGINNER")
+      )
+    );
+    this.advancedCourses$ = courses$.pipe(
+      map((courses) =>
+        courses.filter((course) => course.category == "ADVANCED")
+      )
+    );
   }
 }
--- src/app/services/courses.services.ts
+++ src/app/services/courses.services.ts
@@ -2,6 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { Course } from "../model/course";
+import { map, shareReplay } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root",
@@ -10,6 +11,14 @@ export class CoursesService {
   constructor(private http: HttpClient) {}
 
   loadAllCourses(): Observable<Course[]> {
-    return this.http.get<Course[]>("/api/course");
+    return this.http
+      .get<Course[]>("/api/courses")
+      .pipe(map((res) => res["payload"]));
+  }
+
+  saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
+    return this.http
+      .put(`/api/courses/${courseId}`, changes)
+      .pipe(shareReplay());
   }
 }
