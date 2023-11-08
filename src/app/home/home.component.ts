d08c078fc5d3b09b571679ebe4010d4ef8270de4
Aliz <alicja_bobko@o2.pl>
2023-11-08 15:25:24 +0100

    loading indicator service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,6 +16,7 @@ import { HttpClient } from "@angular/common/http";
 import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { CourseDialogComponent } from "../course-dialog/course-dialog.component";
 import { CoursesService } from "../services/courses.services";
+import { LoadingService } from "../loading/loading.service";
 
 @Component({
   selector: "home",
@@ -27,7 +28,10 @@ export class HomeComponent implements OnInit {
 
   advancedCourses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {}
+  constructor(
+    private coursesService: CoursesService,
+    private loadingService: LoadingService
+  ) {}
 
   ngOnInit() {
     this.reloadCourses();
@@ -36,14 +40,16 @@ export class HomeComponent implements OnInit {
   reloadCourses() {
     const courses$ = this.coursesService
       .loadAllCourses()
-      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo), shareReplay()));
+      .pipe(map((courses) => courses.sort(sortCoursesBySeqNo)));
 
-    this.beginnerCourses$ = courses$.pipe(
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
+
+    this.beginnerCourses$ = loadCourses$.pipe(
       map((courses) =>
         courses.filter((course) => course.category == "BEGINNER")
       )
     );
-    this.advancedCourses$ = courses$.pipe(
+    this.advancedCourses$ = loadCourses$.pipe(
       map((courses) =>
         courses.filter((course) => course.category == "ADVANCED")
       )
